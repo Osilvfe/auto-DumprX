7ac9e3172f0bc3e8c00fb6e162c2866045482ddd
Osilvfe <64585305+Osilvfe@users.noreply.github.com>
2024-07-12 22:03:03 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,16 @@
+curl --parallel --parallel-immediate -k -L -C - -o zzz.ozip 'https://coloroswebsitefs.coloros.com/coloroswebsite-coloros-com/grs_20211102095334/PECM30_11_OTA_0100_all_yTTvpuXCnSQ1.ozip' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: _hjSessionUser_2052333=eyJpZCI6ImRkODVmZjkzLTM3ZDMtNWI1YS1iNDI1LTkwZTc5YjQ4NTljOCIsImNyZWF0ZWQiOjE3MDQwMDc4MjA2OTcsImV4aXN0aW5nIjp0cnVlfQ==; _ga_025F6DCW6B=GS1.1.1704007819.1.1.1704008686.35.0.0; Hm_lvt_1654f286dac930a868f0823c966f217e=1707134942,1708066192; _gcl_au=1.1.505726907.1720407412; _ga=GA1.1.536587355.1704007820; _ga_VD2VD3WEY3=GS1.1.1720407414.7.1.1720407749.10.0.0; Hm_lvt_a917ebf46c87d5881a1ef8c4a2946afc=1720332168,1720523730,1720747109,1720782407; HMACCOUNT=2DFDF1D9BA2D7A61; Hm_lpvt_a917ebf46c87d5881a1ef8c4a2946afc=1720791424' \
+  -H 'DNT: 1' \
+  -H 'Referer: https://www.coloros.com/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-site' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0' \
+  -H 'sec-ch-ua: "Not/A)Brand";v="8", "Chromium";v="126", "Microsoft Edge";v="126"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"'
